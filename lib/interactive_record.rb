5bbd7db132d24a1c429197ac8cf58e75d03177bd
fasnimo <fasnimo@gmail.com>
2019-09-23 16:25:19 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
@@ -56,7 +57,7 @@ class InteractiveRecord
     def self.find_by(attribute_hash)
         value = attribute_hash.values.first
         formatted_value = value.class == Fixnum ? value : "'#{value}'"
-
+     
         sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
         DB[:conn].execute(sql)
     end
